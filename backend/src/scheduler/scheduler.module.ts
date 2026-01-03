5e6b0cca398f554968ed3193e9169e49960ce0a9
seref.altuntas.ea <seref.altuntas.ea@gmail.com>
2026-01-03 09:36:15 +0000

    bug fix scheduler

--- backend/src/scheduler/scheduler.module.ts
+++ backend/src/scheduler/scheduler.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SchedulerService } from './scheduler.service';
+import { DatabaseModule } from '../common/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [SchedulerService],
 })
 export class SchedulerModule {}
